c54a4f2a83f851aa3ca23c4493d62e9ae1a2d1b6
Nicholasp3435 <nap44794@uga.edu>
2024-04-20 02:22:37 -0400

    feat: make GUI and canvas for epic graphics

--- src/main/java/GraphicsApp.java
+++ src/main/java/GraphicsApp.java
@@ -1,33 +1,48 @@
 
 import javafx.application.Platform;
 import javafx.application.Application;
-
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 public class GraphicsApp extends Application {
 
-    private Label test;
+    private Canvas canvas;
+    private Label inLbl;
+    private TextField inField;
+    private HBox inBox;
+    private Button inBtn;
+    private VBox root;
     private Stage stage;
     private Scene scene;
 
     public GraphicsApp() {
 	this.stage = null;
 	this.scene = null;
-	this.test = new Label("test");
+	this.canvas = new Canvas(512, 256);
+	this.root = new VBox();
+	this.inBox = new HBox(5);
+	this.inLbl = new Label("Enter any string and press enter");
+	this.inField = new TextField("Nic");
+	this.inBtn = new Button("Click me to continue");
     } // GraphicsApp
 
     @Override
     public void init() {
 	System.out.println("init() called");
+	connectNodes();
     } // init
 
     @Override
     public void start(Stage stage) {
         this.stage = stage;
-        this.scene = new Scene(this.test);
+        this.scene = new Scene(this.root);
         this.stage.setOnCloseRequest(event -> Platform.exit());
         this.stage.setTitle("test");
         this.stage.setScene(this.scene);
@@ -40,4 +55,10 @@ public class GraphicsApp extends Application {
     public void stop() {
 	System.out.println("stop() called");
     } // stop
+
+    public void connectNodes() {
+	this.inBox.getChildren().addAll(this.inLbl, this.inField, this.inBtn);
+
+	this.root.getChildren().addAll(this.inBox, this.canvas);
+    } // connectNodes
 } // GraphicsApp
